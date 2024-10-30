99571792a3f3b535a0e18f577006aeb18bf271bb
Bosko Pajkic <bpajkic@ryerson.ca>
2024-10-30 18:47:50 -0400

    User data gets pulled from supabase

--- CPS845/src/App.jsx
+++ CPS845/src/App.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import LoginPage from './LoginPage';
 import HomePage from './HomePage'; // Rename App content as HomePage
-import supabase from '../supabaseClient';
+
 
 import './App.css';
 
 function App() {
-  console.log(supabase)
+
 
   return (
     <Routes>
--- CPS845/src/LoginPage.jsx
+++ CPS845/src/LoginPage.jsx
@@ -1,9 +1,39 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './LoginPage.css';
 import './index.css'
 
+//Database Stuff
+import supabase from '../supabaseClient';
+
+
 function LoginPage() {
+  
+  //Fetching Users
+  const [fetchError, setFetchError] = useState(null)
+  const [users, setUsers] = useState(null)
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const { data, error } = await supabase
+        .from('USERS') //Name of table
+        .select()
+
+        if (error) {
+          setFetchError('Could not fetch users')
+          setUsers(null) // setsUsers to null if error occured incase it had data from before
+          console.log(error)
+        }
+        if (data) {
+          setUsers(data)
+          setFetchError(null)
+        }
+    }
+
+    fetchUsers()
+
+  }, [])
+  //Fetching Users End
   
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
@@ -21,6 +51,24 @@ function LoginPage() {
 
   return (
     <div>
+
+      {/* BoskoTesting */}
+      <div>
+        <b>
+          Bosko was here 😱 <br></br>
+          Please use one of the following accounts:
+        </b>
+        {fetchError && (<p>{fetchError}</p>)}
+        {users && (
+          <div className="users">
+            {users.map(user => (
+              <p>Username: {user.USER_NAME} Password: {user.PASSWORD}</p>
+            ))}
+          </div>
+        )}
+      </div>
+      {/* BoskoTestingEnd */}
+
       <h1>SLAP</h1>
       <h4>Gould Street University</h4>
     <div className="login-container">
