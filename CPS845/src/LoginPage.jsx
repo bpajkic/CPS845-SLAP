832d790992462e420a1b970eea2ffe0aa907b24c
Bosko Pajkic <bpajkic@ryerson.ca>
2024-11-10 14:33:28 -0500

    Removed Testing Files, Edited ReadMe, Combines main.jsx and App.jsx

--- CPS845/src/LoginPage.jsx
+++ CPS845/src/LoginPage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './LoginPage.css';
-import './index.css'
+import './main.css'
 
 //Database Stuff
 import supabase from '../supabaseClient';
--- CPS845/src/main.jsx
+++ CPS845/src/main.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import './index.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import LoginPage from './LoginPage';
+import HomePage from './HomePage'; 
+import ViewCoursesPage from './ViewCoursesPage';
+import CoursePage from './CoursePage';
+import CourseProjectsPage from './CourseProjectsPage';
+import CourseSubmissionsPage from './CourseSubmissionsPage';
+import CourseEvaluationsPage from './CourseEvaluationsPage';
+import ProjectInstructions from './ProjectInstructions';
+import './main.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/home/ViewCourses" element={<ViewCoursesPage />} />
+        <Route path="/courses/:id" element={<CoursePage />} />
+        <Route path="/courses/:id/projects" element={<CourseProjectsPage />} />
+        <Route path="/courses/:id/projects/:project_id" element={<ProjectInstructions />} />
+        <Route path="/courses/:id/submissions" element={<CourseSubmissionsPage />} />
+        <Route path="/courses/:id/evaluations" element={<CourseEvaluationsPage />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
