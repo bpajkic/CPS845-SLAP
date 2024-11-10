a69d0c07e278a22a962e2f0738468b2ff0bca48a
Bosko Pajkic <bpajkic@ryerson.ca>
2024-11-10 15:29:38 -0500

    test?

--- CPS845/src/HomePage.jsx
+++ CPS845/src/HomePage.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import "./HomePage.css";
 
 function HomePage() {
--- CPS845/src/LoginPage.jsx
+++ CPS845/src/LoginPage.jsx
@@ -1,57 +1,53 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import './LoginPage.css';
-import './main.css'
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./LoginPage.css";
+import "./main.css";
 
 //Database Stuff
-import supabase from '../supabaseClient';
-
+import supabase from "../supabaseClient";
 
 function LoginPage() {
-  
   //Fetching Users
-  const [fetchError, setFetchError] = useState(null)
-  const [users, setUsers] = useState(null)
+  const [fetchError, setFetchError] = useState(null);
+  const [users, setUsers] = useState(null);
 
   useEffect(() => {
     const fetchUsers = async () => {
       const { data, error } = await supabase
-        .from('USERS') //Name of table
-        .select()
+        .from("USERS") //Name of table
+        .select();
 
-        if (error) {
-          setFetchError('Could not fetch users')
-          setUsers(null) // setsUsers to null if error occured incase it had data from before
-          console.log(error)
-        }
-        if (data) {
-          setUsers(data)
-          setFetchError(null)
-        }
-    }
+      if (error) {
+        setFetchError("Could not fetch users");
+        setUsers(null); // setsUsers to null if error occured incase it had data from before
+        console.log(error);
+      }
+      if (data) {
+        setUsers(data);
+        setFetchError(null);
+      }
+    };
 
-    fetchUsers()
-
-  }, [])
+    fetchUsers();
+  }, []);
   //Fetching Users End
-  
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
   const handleLogin = () => {
     // Basic login validation
     if (username && password) {
       // Redirect to homepage after "login"
-      navigate('/home');
+      navigate("/home");
     } else {
-      alert('Please enter both username and password.');
+      alert("Please enter both username and password.");
     }
   };
 
   return (
     <div>
-
       {/* BoskoTesting */}
       <div>
         <b>
@@ -59,11 +55,13 @@ function LoginPage() {
           Dharshini was here too 😱 <br></br>
           Please use one of the following accounts:
         </b>
-        {fetchError && (<p>{fetchError}</p>)}
+        {fetchError && <p>{fetchError}</p>}
         {users && (
           <div className="users">
-            {users.map(user => (
-              <p>Username: {user.USER_NAME} Password: {user.PASSWORD}</p>
+            {users.map((user) => (
+              <p>
+                Username: {user.USER_NAME} Password: {user.PASSWORD}
+              </p>
             ))}
           </div>
         )}
@@ -72,22 +70,22 @@ function LoginPage() {
 
       <h1>SLAP</h1>
       <h4>Gould Street University</h4>
-    <div className="login-container">
-      <h2>Login</h2>
-      <input
-        type="text"
-        placeholder="Username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={handleLogin}>Login</button>
-    </div>
+      <div className="login-container">
+        <h2>Login</h2>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button onClick={handleLogin}>Login</button>
+      </div>
     </div>
   );
 }
