e28e8928413dbcc0e1c5e584ec239c99c1c11133
Bosko Pajkic <bpajkic@ryerson.ca>
2024-11-11 17:02:19 -0500

    Template Page Stuff done

--- CPS845/src/TemplatePage.jsx
+++ CPS845/src/TemplatePage.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useParams, Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./main.css";
 
 function TemplatePage({ children }) {
