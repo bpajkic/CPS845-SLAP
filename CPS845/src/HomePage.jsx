111dd65b98b977a6500de717458dabc07acf7ad3
imanqureshi <imanqureshi03@gmail.com>
2024-11-11 22:50:56 -0500

    UI design changes

--- CPS845/src/HomePage.jsx
+++ CPS845/src/HomePage.jsx
@@ -8,14 +8,18 @@ function HomePage() {
           <section className="main-content">
             <div className="calendar">
               <h2>Calendar</h2>
-              <p>- project 1 due Friday</p>
+              <ul className="lists">
+                <li>project 1 due Friday</li>
+              </ul>
             </div>
           </section>
           <section className="main-content">
             <div className="slap-notifications">
               <h2>SLAPs</h2>
-              <p>- System will be down Saturday from 6am-8am</p>
-              <p>- New Project in class 3</p>
+              <ul  className="lists">
+                <li>System will be down Saturday from 6am-8am</li>
+                <li>New Project in class 3</li>
+              </ul>
             </div>
           </section>
       </TemplatePage>
--- CPS845/src/TemplatePage.jsx
+++ CPS845/src/TemplatePage.jsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import supabase from '../supabaseClient';
+import supabase from "../supabaseClient";
 import "./main.css";
 
 function TemplatePage({ children }) {
@@ -15,12 +15,10 @@ function TemplatePage({ children }) {
 
   useEffect(() => {
     const fetchCourses = async () => {
-      const { data, error } = await supabase
-        .from('COURSES') 
-        .select();
+      const { data, error } = await supabase.from("COURSES").select();
 
       if (error) {
-        setFetchError('Could not fetch courses');
+        setFetchError("Could not fetch courses");
         setCourses([]);
         console.error(error);
       } else {
@@ -32,59 +30,59 @@ function TemplatePage({ children }) {
     fetchCourses();
   }, []);
 
-
   return (
-    <div className="container">
-      {/* sidebar */}
-      <aside className="sidebar">
-        <h2>Menu</h2>
-        <nav className="menu">
-          <ul className="submenu"></ul>
-          <ul>
-            <li className="submenu-item">
-              <Link to="/home">Home</Link>
-            </li>
-            <li className="submenu-item">
-              <Link to="/home/ViewCourses">View Courses</Link>
-            </li>
-
-            {/* List of Courses for the Side Menu */}
-            {fetchError && <p className="error">{fetchError}</p>}
-            {courses.map(course => (
-                <li key={course.id} className="submenu-item">
-                <Link to={`/courses/${course.id}`}>{course.COURSE_CODE}</Link>
+    <div>
+      <header className="header">
+        <div className="title">
+          <h1>SLAP Interface</h1>
+          <h3>Gould Street University</h3>
+        </div>
+        <div>
+          <button className="logout" onClick={handleLogout}>
+            LOGOUT
+          </button>
+        </div>
+      </header>
+      <div className="container">
+        {/* sidebar */}
+        <aside className="sidebar">
+          <h2>Menu</h2>
+          <nav className="menu">
+            <ul className="submenu"></ul>
+            <ul>
+              <li className="submenu-item">
+                <Link to="/home">Home</Link>
               </li>
-            ))}
-
-          </ul>
-        </nav>
-        <h2 id="chat">Chat</h2>
-        <nav className="chat-menu">
-          <ul className="chat-submenu">
-            <li className="submenu-item">class 1 (1)</li>
-            <ul className="chat-options">
-              <li>project 1 (1)</li>
-              <li>John Doe (1)</li>
-              <li>all</li>
-              <li>TA</li>
-              <li>Instructor</li>
+              <li className="submenu-item">
+                <Link to="/home/ViewCourses">View Courses</Link>
+              </li>
+              {/* List of Courses for the Side Menu */}
+              {fetchError && <p className="error">{fetchError}</p>}
+              {courses.map((course) => (
+                <li key={course.id} className="course-options">
+                  <Link to={`/courses/${course.id}`}>{course.COURSE_CODE}</Link>
+                </li>
+              ))}
             </ul>
-            <li className="submenu-item">class 2</li>
-            <li className="submenu-item">class 3</li>
-          </ul>
-        </nav>
-      </aside>
-      {/* main */}
-      <div>
+          </nav>
+          <h2 id="chat">Chat</h2>
+          <nav className="chat-menu">
+            <ul className="chat-submenu">
+              <li className="submenu-item">class 1 (1)</li>
+              <ul className="chat-options">
+                <li>project 1 (1)</li>
+                <li>John Doe (1)</li>
+                <li>all</li>
+                <li>TA</li>
+                <li>Instructor</li>
+              </ul>
+              <li className="submenu-item">class 2</li>
+              <li className="submenu-item">class 3</li>
+            </ul>
+          </nav>
+        </aside>
+        {/* main */}
         <main className="content">
-          <header className="header">
-            <h1>SLAP Interface</h1>
-            <button className="logout" onClick={handleLogout}>
-              LOGOUT
-            </button>
-          </header>
-          <h4>Gould Street University</h4>
-
           {/* MAIN CONTENT GOES HERE */}
           {children}
         </main>
